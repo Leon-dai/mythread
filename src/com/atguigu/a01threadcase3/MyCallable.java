07f2b66cecc9204ae2e0efd145197db4c9673988
Leon <212649354@qq.com>
2024-02-08 14:37:03 +0800

    多线程的第三种启动方式

---
+++ src/com/atguigu/a01threadcase3/MyCallable.java
@@ -0,0 +1,16 @@
+package com.atguigu.a01threadcase3;
+
+import java.util.concurrent.Callable;
+
+public class MyCallable implements Callable<Integer> {
+
+    @Override
+    public Integer call() throws Exception {
+        //求1~100的和
+        int sum = 0;
+        for (int i = 0; i <= 100; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+}
