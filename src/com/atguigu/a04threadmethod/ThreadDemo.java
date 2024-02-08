91bb4fb4da9bcbb94383807eeeb6356dffd900ad
Leon <212649354@qq.com>
2024-02-08 17:15:29 +0800

    多线程的常用方法

---
+++ src/com/atguigu/a04threadmethod/ThreadDemo.java
@@ -0,0 +1,17 @@
+package com.atguigu.a04threadmethod;
+
+public class ThreadDemo {
+    public static void main(String[] args) {
+        //1.创建线程的对象
+        MyThread t1 = new MyThread("飞机");
+        MyThread t2 = new MyThread("坦克");
+
+////        开启线程
+//        t1.start();
+//        t2.start();
+
+        System.out.println(t1.getPriority());
+        System.out.println(t2.getPriority());
+        System.out.println(Thread.currentThread().getPriority());
+    }
+}
